d447e2efb717851ce757cc24c8a1b384288a4656
AswinBarath <aswin2001barath@gmail.com>
2021-09-22 20:01:40 +0530

    Refactor

--- sde_sheet/LongestConsecutiveSequence.java
+++ sde_sheet/LongestConsecutiveSequence.java
@@ -1,4 +1,4 @@
-package hashing;
+package sde_sheet;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -6,32 +6,32 @@ import java.util.Set;
 public class LongestConsecutiveSequence {
 
 	public static void main(String[] args) {
-		int arr[] = {0,3,7,2,5,8,4,6,0,1};
+		int arr[] = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
 		System.out.println(longestConsecutiveSequence(arr));
 	}
-	
+
 	public static int longestConsecutiveSequence(int[] nums) {
 		Set<Integer> hashSet = new HashSet<Integer>();
-		for(int num : nums) {
+		for (int num : nums) {
 			hashSet.add(num);
 		}
-		
+
 		int longestStreak = 0;
-		
-		for(int num : nums) {
-			if(!hashSet.contains(num-1)) {
+
+		for (int num : nums) {
+			if (!hashSet.contains(num - 1)) {
 				int currentNum = num;
 				int currentStreak = 1;
-				
-				while(hashSet.contains(currentNum+1)) {
+
+				while (hashSet.contains(currentNum + 1)) {
 					currentNum += 1;
 					currentStreak += 1;
 				}
-				
+
 				longestStreak = Math.max(longestStreak, currentStreak);
 			}
 		}
-		
+
 		return longestStreak;
 	}
 
--- sde_sheet/TwoSum.java
+++ sde_sheet/TwoSum.java
@@ -1,4 +1,4 @@
-package hashing;
+package sde_sheet;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -7,19 +7,19 @@ public class TwoSum {
 
 	public static void main(String[] args) {
 
-		int[] nums = {2,7,11,15};
+		int[] nums = { 2, 7, 11, 15 };
 		int target = 9;
 		int[] res = twoSum(nums, target);
-		for(int num : res) {
+		for (int num : res) {
 			System.out.print(num + " ");
 		}
 	}
-	
+
 	public static int[] twoSum(int[] nums, int target) {
 		int[] result = new int[2];
 		Map<Integer, Integer> map = new HashMap<>();
-		for(int i=0; i<nums.length; i++) {
-			if(map.containsKey(target - nums[i])) {
+		for (int i = 0; i < nums.length; i++) {
+			if (map.containsKey(target - nums[i])) {
 				result[1] = i;
 				result[0] = map.get(target - nums[i]);
 				return result;
