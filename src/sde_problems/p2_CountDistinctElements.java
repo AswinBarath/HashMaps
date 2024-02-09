6ed118d36d0e1ee660c1c57b83ae79b43fec4edc
AswinBarath <aswin2001barath@gmail.com>
2024-02-09 09:26:08 +0530

    Update Count Distinct Elements Code

--- src/sde_problems/p2_CountDistinctElements.java
+++ src/sde_problems/p2_CountDistinctElements.java
@@ -2,32 +2,29 @@ package sde_problems;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class p2_CountDistinctElements {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        Solution s = new Solution();
+        Solution1 s = new Solution1();
 
         ArrayList<Integer> A = new ArrayList<>();
 
         // Input 1
         A.addAll(Arrays.asList(3, 4, 3, 6, 6));
-        ArrayList<Integer> soln1 = s.solve(A);
-        // Print all values in one line
-        System.out.println("Answer ArrayList: " + soln1);
+        System.out.println("Answer: " + s.solve(A));
         A.clear();
 
         // Input 2
         A.addAll(Arrays.asList(3, 3, 3, 9, 0, 1, 0));
-        ArrayList<Integer> soln2 = s.solve(A);
-        // Print all values in one line
-        System.out.println("Answer ArrayList: " + soln2);
+        System.out.println("Answer: " + s.solve(A));
         A.clear();
 
     }
 }
 
-class Solution {
+class Solution1 {
     public int solve(ArrayList<Integer> A) {
         HashSet<Integer> distinct = new HashSet<>();
         for(int a : A) {
